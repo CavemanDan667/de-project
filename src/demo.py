aed98717fcc68179ee68e4e8f6bb00e0da2b2852
Wander1ngPanda <jack@ukmurphy.com>
2023-10-30 15:58:55 +0000

    Terraform: Now flake-8 compliant

--- src/demo.py
+++ src/demo.py
@@ -3,5 +3,6 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
 def handler(event, context):
-    logger.info('Well, that worked.')
+    logger.info("Well, that worked.")
