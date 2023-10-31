90abe869200640af6ddcffe683cb4b69b19d6dc7
Helen Lyttle <helenlyttle@live.co.uk>
2023-10-31 16:37:52 +0000

    created and tested extract_newest_time function

---
+++ src/extract_newest_time.py
@@ -0,0 +1,34 @@
+import re
+
+
+def extract_newest_time(filenames):
+    """This function takes a list of filenames,
+    extracts timestamps from them using regex,
+    sorts them into order,
+    and returns the largest one.
+
+    Args:
+        filenames(list):
+            will be returned out of a function that
+            queries the contents of an AWS s3 bucket.
+            Each filename will consist of a string
+            pseudo-directory name, followed by a forward slash,
+            a string of numbers and a .csv extension.
+
+    Returns:
+        newest_time(int):
+            a number representing the most recent unix time
+            found in the file names.
+
+    Raises:
+        TypeError if passed an argument with value of None.
+        """
+    newest_time = 0
+    for file in filenames:
+        try:
+            file_time = int(re.search("(?<=/)([0-9]+)(?=.csv)", file).group())
+            if file_time > newest_time:
+                newest_time = file_time
+        except AttributeError:
+            pass
+    return newest_time
