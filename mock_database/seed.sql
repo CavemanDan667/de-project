fa9b9a88a05714f059840fb9537c3cbce4528152
Helen Lyttle <helenlyttle@live.co.uk>
2023-11-01 12:26:11 +0000

    created and tested fetch_data function

--- mock_database/seed.sql
+++ mock_database/seed.sql
@@ -8,19 +8,23 @@ CREATE DATABASE mock_tote_db;
 CREATE TABLE table_1 (
     column_1 INT,
     column_2 VARCHAR(50),
-    column_3 INT
+    column_3 INT,
+    last_updated TIMESTAMP
 );
 
-INSERT INTO table_1 (column_1, column_2, column_3)
-VALUES (1, 'one', 10),
-        (2, 'two', 20),
-        (3, 'three', 30);
+INSERT INTO table_1 (column_1, column_2, column_3, last_updated)
+VALUES (1, 'one', 10, '2020-01-01 00:00:00'),
+        (2, 'two', 20, '2020-01-01 00:00:00'),
+        (3, 'three', 30, '2023-11-01 00:00:00');
 
 CREATE TABLE table_2 (
     column_a INT,
     column_b VARCHAR(50),
-    column_c INT
+    column_c INT,
+    last_updated TIMESTAMP
 );
 
 CREATE TABLE _table_3 (
 );
+
+SELECT * FROM table_1 WHERE last_updated BETWEEN '2022-01-01 00:00:00' AND '2024-01-01 00:00:00';
--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,14 +1,18 @@
-from pg8000.native import DatabaseError
+from pg8000.native import DatabaseError, identifier, literal
 
 
-def fetch_data(conn, table_name):
+def fetch_data(conn, table_name, newest_time, time_now):
     """
     This function takes a database connection and a table name,
-    and fetches the data from that table.
+    along with two timestamps,
+    and fetches the data from that table
+    updated between those times.
 
     Args:
         conn: an established connection to a database.
         table_name(string): the name of a table within that database.
+        newest_time: the last timestamp found on a previous file.
+        time_now: a timestamp created in the parent function.
 
     Returns:
         If successful, a dictionary
@@ -19,8 +23,12 @@ def fetch_data(conn, table_name):
         TypeError if parameters are missing.
     """
     try:
-        data = conn.run(f"SELECT * FROM {table_name}")
+        query = f"""SELECT * FROM {identifier(table_name)}
+        WHERE last_updated BETWEEN {literal(newest_time)}
+        AND {literal(time_now)}"""
+        data = conn.run(query)
         headers = [c['name'] for c in conn.columns]
         return {'Headers': headers, 'Rows': data}
-    except DatabaseError:
-        return 'Table not found'
+    except DatabaseError as d:
+        print(d)
+        return f'There was a database error: {table_name}'
