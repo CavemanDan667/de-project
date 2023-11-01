55c03c52a9b1e85fa0e6f6c66549ef1b28d72623
Helen Lyttle <helenlyttle@live.co.uk>
2023-11-01 10:11:34 +0000

    created and tested 'get_tables' function

---
+++ src/utils/get_tables.py
@@ -0,0 +1,24 @@
+def get_table_names(conn):
+    """
+    This function connects to a database
+    and returns a list of all the tables.
+    
+    Args:
+        conn: A connection set up to a specific database,
+        allowing this function to be used on any database.
+    
+    Returns:
+        A list of all table names within that database,
+        if connection is successful.
+    
+    Raises:
+        TypeError if invoked without a connection.
+    """
+    data = conn.run("""SELECT table_name
+                    FROM information_schema.tables
+                    WHERE table_schema='public'
+                    AND table_type='BASE TABLE'
+                    AND table_name NOT LIKE '\\_%';
+                    """)
+    table_list = [item[0] for item in data]
+    return table_list
---
+++ tests/test_get_tables.py
@@ -0,0 +1,41 @@
+from src.utils.get_tables import get_table_names
+import pytest
+from pg8000.native import Connection
+
+
+def test_function_raises_error_if_no_connection_passed():
+    with pytest.raises(TypeError):
+        get_table_names()
+
+
+def test_function_returns_table_names_in_a_list():
+    mock_conn = Connection(
+        user='helen',
+        host='localhost',
+        port=5432,
+        database='mock_tote_db'
+    )
+    result = get_table_names(mock_conn)
+    assert result == ['table_1', 'table_2']
+
+
+def test_function_ignores_private_table_names():
+    mock_conn = Connection(
+        user='helen',
+        host='localhost',
+        port=5432,
+        database='mock_tote_db'
+    )
+    result = get_table_names(mock_conn)
+    assert result == ['table_1', 'table_2']
+
+
+def test_returns_empty_list_if_database_is_empty():
+    empty_conn = Connection(
+        user='helen',
+        host='localhost',
+        port=5432,
+        database='mock_empty_db'
+    )
+    result = get_table_names(empty_conn)
+    assert result == []
