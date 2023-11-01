b35c337523751337860c3fc396f10709f0c7ff06
peterkonst <peterkonst77@gmail.com>
2023-11-01 12:01:40 +0000

    using mocks

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,10 +1,23 @@
 from src.connection import get_connection
 from unittest import TestCase
+from unittest.mock import MagicMock, patch
 from pg8000.exceptions import InterfaceError
 import pytest
 
 
 class TestConnection(TestCase):
+     
+    @patch("src.connection.get_connection")
+    def test_database(self, mock_connect):
+        get_connection(
+            user='peterkonstantynov',
+            password='password',
+            host='127.0.0.1',
+            port=5432,
+            database='test'
+        )
+        mock_connect.return_value = MagicMock()
+        self.assertIsNotNone(mock_connect)
 
     def test_connection_to_database_is_successful_with_correct_configuration(self): # noqa
         con = get_connection(user='peterkonstantynov',
