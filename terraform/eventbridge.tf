2d2bb0e4cea5b2f390327a29c1ff145c4a163d3d
peterkonst <peterkonst77@gmail.com>
2023-11-01 14:51:38 +0000

    created an eventbridge, tested it works!

---
+++ terraform/eventbridge.tf
@@ -0,0 +1,21 @@
+
+resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+    name = "every-five-minutes"
+    description = "Fires every five minutes"
+    schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "check_foo_every_five_minutes" {
+    rule = aws_cloudwatch_event_rule.every_five_minutes.name
+    target_id = "ingestion_lambda"
+    arn = aws_lambda_function.ingestion_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_ingestion_lambda" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.ingestion_lambda.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
+}
+
