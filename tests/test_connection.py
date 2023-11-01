34f4311e31eaf0e202122c223d43406c1d09c9c0
peterkonst <peterkonst77@gmail.com>
2023-11-01 09:59:34 +0000

    sorting error

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -9,28 +9,18 @@ class TestConnection(TestCase):
 
     def test_connection_to_database_is_successful_with_correct_configuration(self): # noqa
         config = dotenv_values(".env")
-        user = config["USER"]
-        password = config["PASSWORD"]
-        host = config["HOST"]
-        port = config["PORT"]
-        database = config["DATABASE"]
-        con = get_connection(user=user,
-                             password=password,
-                             host=host,
-                             port=port,
-                             database=database)
+        con = get_connection(user=config["USER"],
+                             password=config["PASSWORD"],
+                             host=config["HOST"],
+                             port=config["PORT"],
+                             database=config["DATABASE"])
         self.assertIsNotNone(con)
         con.close()
 
     def test_connection_raises_interface_error_when_incorrect_configuration_used(self): # noqa
-        user = "jason"
-        password = "password123"
-        host = "nc-database"
-        port = 1234
-        database = "db"
         with pytest.raises(InterfaceError):
-            get_connection(user=user,
-                           password=password,
-                           host=host,
-                           port=port,
-                           database=database)
+            get_connection(user="jason",
+                           password="password123",
+                           host="nc-database",
+                           port=1234,
+                           database="db")
