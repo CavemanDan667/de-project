e436c5af01d9d6cb598cac3c6fbe67390068420e
peterkonst <peterkonst77@gmail.com>
2023-11-01 09:42:11 +0000

    finished with testing the connection

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,20 +1,36 @@
 from src.connection import get_connection
 from unittest import TestCase
-from pg8000.native import Connection
 from pg8000.exceptions import InterfaceError
 import pytest
+from dotenv import dotenv_values
 
 
 class TestConnection(TestCase):
+
     def test_connection_to_database_is_successful_with_correct_configuration(self): # noqa
-        con = get_connection()
+        config = dotenv_values(".env")
+        user = config["USER"]
+        password = config["PASSWORD"]
+        host = config["HOST"]
+        port = config["PORT"]
+        database = config["DATABASE"]
+        con = get_connection(user=user,
+                             password=password,
+                             host=host,
+                             port=port,
+                             database=database)
         self.assertIsNotNone(con)
         con.close()
 
     def test_connection_raises_interface_error_when_incorrect_configuration_used(self): # noqa
+        user = "jason"
+        password = "password123"
+        host = "nc-database"
+        port = 1234
+        database = "db"
         with pytest.raises(InterfaceError):
-            Connection(user="jason",
-                       password="password123",
-                       host="nc-database",
-                       port=1234,
-                       database="db")
+            get_connection(user=user,
+                           password=password,
+                           host=host,
+                           port=port,
+                           database=database)
