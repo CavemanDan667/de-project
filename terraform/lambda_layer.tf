5cf19616608ac2a9485b473afef631141e9a64c0
Wander1ngPanda <jack@ukmurphy.com>
2023-11-03 12:12:22 +0000

    Terraform Deployment

--- terraform/lambda_layer.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_lambda_layer_version" "shared_layer" {
-    filename = "ingestion_utils.zip"
-    description = "Ingestion Shared Lambda Layer"
-    layer_name = "project-shared-layer"
-}
-
