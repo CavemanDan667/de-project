4aeee09580686108be768fd33b1f84ab18e4a5c6
Jason <jasoncolville@hotmail.co.uk>
2023-11-03 13:37:35 +0000

    flake compliance

--- src/process/process_playground.py
+++ src/process/process_playground.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-
 
 transaction_df = pd.read_csv("tests/csv_test_files/test-transaction.csv")
 purchase_df = pd.read_csv("tests/csv_test_files/test-purchase-order.csv")
@@ -9,9 +7,11 @@ print(transaction_df)
 print(purchase_df)
 print(sales_df)
 
-purchase_transactions_df = pd.merge(transaction_df, purchase_df, on="purchase_order_id")
+purchase_transactions_df = pd.merge(transaction_df,
+                                    purchase_df, on="purchase_order_id")
 
-sales_transactions_df = pd.merge(transaction_df, sales_df, on="sales_order_id")
+sales_transactions_df = pd.merge(transaction_df,
+                                 sales_df, on="sales_order_id")
 
 print(purchase_transactions_df)
 print(sales_transactions_df)
