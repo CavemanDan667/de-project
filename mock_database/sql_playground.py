d87df807d36e50b6d3072ea9052a5a58453ddedf
Wander1ngPanda <jack@ukmurphy.com>
2023-11-06 10:20:27 +0000

    Diagram of the fact sales table created

---
+++ mock_database/sql_playground.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+# from pg8000.native import Connection
+
+
+# def test_make_dim_design():
+    # conn = Connection(user="mock_tote_db",
+    #                   host="localhost",
+    #                   password="postgres")
+    # conn.run("DROP TABLE IF EXISTS dim_design;")
+    # conn.run("CREATE TABLE dim_design (design_ID serial primary key, design_name VARCHAR(50), column_3 INT, last_updated TIMESTAMP);")
+    # conn.run("INSERT INTO dim_design (column_1, column_2, column_3, last_updated) VALUES (1, 'one', 10, '2020-01-01 00:00:00'), (2, 'two', 20, '2020-01-01 00:00:00'), (3, 'three', 30, '2023-11-01 00:00:00');")
+    
+
+# test_db_seeder()
