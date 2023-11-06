0bdfc2a12405c33b66aef50c33eb9b2008ff4f4c
Helen Lyttle <helenlyttle@live.co.uk>
2023-11-06 16:33:28 +0000

    more work on transform_currency

--- src/process/transform_currency.py
+++ src/process/transform_currency.py
@@ -14,7 +14,9 @@ def transform_currency(csv_file, conn):
     df = pd.DataFrame.from_dict(currency_dict)
     for value in df.values.tolist():
         try:
-            query = f'INSERT INTO dim_currency (currency_code, currency_name) VALUES ({literal(value[0])}, {literal(value[1])});'
+            query = f'''INSERT INTO dim_currency
+              (currency_code, currency_name)
+                VALUES ({literal(value[0])}, {literal(value[1])});'''
             conn.run(query)
         except DatabaseError as d:
             raise d
