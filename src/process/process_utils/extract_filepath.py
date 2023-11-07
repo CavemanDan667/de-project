bf65f2a6df122696c358bec91849cb3cee74e469
Wander1ngPanda <jack@ukmurphy.com>
2023-11-07 10:59:37 +0000

    extract event data created

--- src/process/process_utils/extract_filepath.py
+++ src/process/process_utils/extract_filepath.py
@@ -6,11 +6,11 @@ def extract_filepath(event):
     """Extracts S3 URI from event JSON
 
     Args:
-        event (dictionary): The notification message that Amazon 
+        event (dictionary): The notification message that Amazon
         S3 sends when an object is created. In JSON format.
 
     Returns:
-        string: The S3 URI of the object referenced in the 
+        string: The S3 URI of the object referenced in the
         given event JSON. Has the form "s3://bucket/key"
     """
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
