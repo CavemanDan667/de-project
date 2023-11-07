f50a16096a7a6471868bd1d3f822c9a2adf2e2fe
Helen Lyttle <helenlyttle@live.co.uk>
2023-11-07 10:31:12 +0000

    flake8 compliance

--- src/process/process_utils/write_to_parquet.py
+++ src/process/process_utils/write_to_parquet.py
@@ -1,4 +1,3 @@
-import pandas
 import logging
 import boto3
 
@@ -14,10 +13,11 @@ logger.setLevel(logging.INFO)
 
 def write_data_to_parquet(now, table_name, data_frame):
     data_frame.to_parquet("/tmp/data.parquet")
-    upload_object(now, table_name,'data.parquet')
+    upload_object(now, table_name, 'data.parquet')
+
 
 def upload_object(now, table_name, file_name):
-        s3 = get_client("s3")
-        s3.upload_file(
-            file_name, "de-project-processed-bucket", f"{table_name}/{now}.csv"
-        )
+    s3 = get_client("s3")
+    s3.upload_file(
+        file_name, "de-project-processed-bucket", f"{table_name}/{now}.csv"
+    )
