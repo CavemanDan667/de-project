c666149bc08f55e7c5251b20e36ae9cfb6326f25
Wander1ngPanda <jack@ukmurphy.com>
2023-11-07 11:29:09 +0000

    Flake compliance

--- src/process/process.py
+++ src/process/process.py
@@ -1,7 +1,8 @@
 import os
 import logging
 from process_utils.write_to_parquet import write_data_to_parquet
-
+from process_utils.extract_filepath import extract_filepath
+from process_utils.extract_event_data import extract_event_data
 
 user = os.environ["user"]
 host = os.environ["host"]
@@ -15,8 +16,8 @@ logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-    file_name = event['Records'][0]['s3']['object']['key']
-    now = file_name.split('/')[1][:-4]
-    table = file_name.split('/')[0]
-    data_frame = 'some function'
-    write_data_to_parquet(now, table, data_frame)
+    table_name, unix = extract_event_data(event)
+    file_path = extract_filepath(event)
+    print(file_path)
+    data_frame = "some function"
+    write_data_to_parquet(unix, table_name, data_frame)
