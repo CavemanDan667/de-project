f802158d3de20a95597d1a8c0da4216d42f4fe77
Helen Lyttle <helenlyttle@live.co.uk>
2023-11-07 11:36:48 +0000

    further yaml updates

--- src/process/process.py
+++ src/process/process.py
@@ -1,19 +1,23 @@
 import os
 import logging
 from process_utils.write_to_parquet import write_data_to_parquet
+from process_utils.extract_filepath import extract_filepath
+from process_utils.extract_event_data import extract_event_data
+
 user = os.environ["user"]
 host = os.environ["host"]
 database = os.environ["database"]
 password = os.environ["password"]
 port = os.environ["port"]
 
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
 def handler(event, context):
-    file_name = event['Records'][0]['s3']['object']['key']
-    now = file_name.split('/')[1][:-4]
-    table = file_name.split('/')[0]
-    data_frame = 'some function'
-    write_data_to_parquet(now, table, data_frame)
-    
+    table_name, unix = extract_event_data(event)
+    file_path = extract_filepath(event)
+    print(file_path)
+    data_frame = "some function"
+    write_data_to_parquet(unix, table_name, data_frame)
