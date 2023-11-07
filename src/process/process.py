c9e78b6c15a00a2a566014c0b2f88be7846cbe92
Wander1ngPanda <jack@ukmurphy.com>
2023-11-07 15:26:48 +0000

    Process handler tested and error handling

--- src/process/process.py
+++ src/process/process.py
@@ -1,4 +1,3 @@
-import os
 import logging
 from pg8000.native import Connection
 from src.process.process_utils.write_to_parquet import write_data_to_parquet
@@ -6,12 +5,15 @@ from src.process.process_utils.extract_filepath import extract_filepath
 from src.process.process_utils.extract_event_data import extract_event_data
 from src.process.transform_currency import transform_currency
 from src.process.transform_design import transform_design
+from dotenv import dotenv_values
 
-user = os.environ["DW_USER"]
-host = os.environ["DW_HOST"]
-database = os.environ["DW_DATABASE"]
-password = os.environ["DW_PASSWORD"]
-port = os.environ["DW_PORT"]
+config = dotenv_values(".env")
+
+user = config["DW_USER"]
+host = config["DW_HOST"]
+database = config["DW_DATABASE"]
+password = config["DW_PASSWORD"]
+port = config["DW_PORT"]
 
 
 logger = logging.getLogger(__name__)
@@ -19,21 +21,26 @@ logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-    
-    conn = Connection(
-            user=user,
-            host=host,
-            database=database,
-            port=port,
-            password=password
-        )
-    
-    table_name, unix = extract_event_data(event)
-    file_path = extract_filepath(event)
-    
-    if table_name == 'currency':
-        data_frame = transform_currency(conn, file_path)
-    elif table_name == 'design':
-        data_frame = transform_design(conn, file_path)
+    try:
+        conn = Connection(
+                user=user,
+                host=host,
+                database=database,
+                port=port,
+                password=password
+            )
 
-    write_data_to_parquet(unix, table_name, data_frame)
+        data_frame = None
+
+        table_name, unix = extract_event_data(event)
+        file_path = extract_filepath(event)
+
+        if table_name == 'currency':
+            data_frame = transform_currency(conn, file_path)
+        elif table_name == 'design':
+            data_frame = transform_design(conn, file_path)
+
+        write_data_to_parquet(unix, table_name, data_frame)
+    except Exception as e:
+        logger.error(f'Process handler has raised an error: {e}')
+        raise e
