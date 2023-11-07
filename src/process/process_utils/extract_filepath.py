b2ae03b886fe5e9e37cf56c6d7b4d428e9f5c4d8
CavemanDan667 <dancox82@gmail.com>
2023-11-07 11:12:29 +0000

    added error handling and test to extract_filepath

--- src/process/process_utils/extract_filepath.py
+++ src/process/process_utils/extract_filepath.py
@@ -1,19 +1,30 @@
-# receives an event
-# outputs S3 URI of a bucket object
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def extract_filepath(event):
     """Extracts S3 URI from event JSON
 
     Args:
-        event (dictionary): The notification message that Amazon 
+        event (dictionary): The notification message that Amazon
         S3 sends when an object is created. In JSON format.
 
     Returns:
-        string: The S3 URI of the object referenced in the 
+        string: The S3 URI of the object referenced in the
         given event JSON. Has the form "s3://bucket/key"
+
+    Raises:
+        KeyError
     """
-    bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
-    key = event["Records"][0]["s3"]["object"]["key"]
-    filepath = "s3://" + bucket_name + "/" + key
-    return filepath
+    try:
+        bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+        key = event["Records"][0]["s3"]["object"]["key"]
+        filepath = "s3://" + bucket_name + "/" + key
+        return filepath
+    except KeyError as k:
+        msg = f"extract_filepath has raised KeyError: key not found: {k}"
+        logger.error(msg)
+        raise k
