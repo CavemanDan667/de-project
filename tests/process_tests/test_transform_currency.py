682a6eb7797540c69887e35880c6c90c94544a24
CavemanDan667 <dancox82@gmail.com>
2023-11-07 14:06:29 +0000

    changed created alarm to 5 out of 6 and tweaked test_transform_currency

--- tests/process_tests/test_transform_currency.py
+++ tests/process_tests/test_transform_currency.py
@@ -1,5 +1,5 @@
 from src.process.transform_currency import transform_currency
-from pg8000.native import Connection, InterfaceError
+from pg8000.native import Connection, DatabaseError
 
 from dotenv import dotenv_values
 import pytest
@@ -16,35 +16,28 @@ database = config["TESTDW_DATABASE"]
 @pytest.fixture
 def conn():
     return Connection(
-        user=user,
-        password=password,
-        host=host,
-        port=port,
-        database=database)
+        user=user, password=password, host=host, port=port, database=database
+    )
 
 
 def test_function_returns_data_frame(conn):
-    result = transform_currency(
-       'tests/csv_test_files/test-currency.csv',
-       conn
-    )
+    result = transform_currency("tests/csv_test_files/test-currency.csv", conn)
     assert result.values.tolist() == [
-        ['GBP', 'British pound'],
-        ['USD', 'United States dollar'],
-        ['EUR', 'European Euro']
+        ["GBP", "British pound"],
+        ["USD", "United States dollar"],
+        ["EUR", "European Euro"],
     ]
 
 
-def test_function_raises_interface_error_if_query_fails():
-    conn = Connection(
-        user='user',
-        password='password',
-        host = config["TESTDW_HOST"],
-        port = config["TESTDW_PORT"],
-        database='database'
-    )
-    with pytest.raises(InterfaceError):
+def test_function_raises_database_error_if_query_fails():
+    with pytest.raises(DatabaseError):
         transform_currency(
-            'tests/csv_test_files/test-currency.csv',
-            conn
+            "tests/csv_test_files/test-currency.csv",
+            Connection(
+                user="user",
+                password="password",
+                host=config["TESTDW_HOST"],
+                port=config["TESTDW_PORT"],
+                database="database",
+            ),
         )
