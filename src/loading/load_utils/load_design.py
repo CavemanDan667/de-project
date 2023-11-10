d41319c4611ea6d85c9304b8ab8643945101a864
CavemanDan667 <dancox82@gmail.com>
2023-11-10 12:28:27 +0000

    added logging and another exception to transform_design and load_design

--- src/loading/load_utils/load_design.py
+++ src/loading/load_utils/load_design.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from pg8000.native import DatabaseError, literal
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def load_design(parquet_file, conn):
@@ -23,26 +27,26 @@ def load_design(parquet_file, conn):
     data = pd.read_parquet(parquet_file)
     for row in data.values.tolist():
         try:
-            select_query = f'''SELECT * FROM dim_design
-            WHERE design_id = {literal(row[0])};'''
+            select_query = f"""SELECT * FROM dim_design
+            WHERE design_id = {literal(row[0])};"""
             query_result = conn.run(select_query)
             if len(query_result) == 0:
-                insert_query = f'''INSERT INTO dim_design
+                insert_query = f"""INSERT INTO dim_design
                         (design_id, design_name, file_location, file_name)
                         VALUES
                         ({literal(row[0])},
                          {literal(row[1])},
                          {literal(row[2])},
-                         {literal(row[3])});'''
+                         {literal(row[3])});"""
             elif len(query_result) > 0:
-                insert_query = f'''UPDATE dim_design
+                insert_query = f"""UPDATE dim_design
                 SET design_name = {literal(row[1])},
                 file_location = {literal(row[2])},
                 file_name = {literal(row[3])}
-                WHERE design_id = {literal(row[0])}'''
+                WHERE design_id = {literal(row[0])}"""
             conn.run(insert_query)
         except DatabaseError as d:
             raise d
         except Exception as e:
             raise e
-    return 'Data loaded successfully - dim_design'
+    return "Data loaded successfully - dim_design"
