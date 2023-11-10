4d707e935ea895a02a8a8f1777535fb81c3e4566
CavemanDan667 <dancox82@gmail.com>
2023-11-10 12:18:41 +0000

    transform and load address as well as tests

---
+++ src/loading/load_utils/load_address.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from pg8000.native import DatabaseError, literal
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def load_address(parquet_file, conn):
+    """This function reads a processed parquet file.
+    It then uses a connection to the data warehouse
+    to populate the corresponding dim_location
+    table with the data from that file.
+
+    Args:
+        parquet_file: a filepath to a parquet file containing
+        data processed in a separate function.
+        conn: a connection to the new data warehouse.
+    Returns:
+        a message confirming successful load of data.
+    Raises:
+        DatabaseError: if either the select or insert
+        query fails to match up to the destination
+        table.
+        Exception: if an unexpected error occurs.
+    """
+    data = pd.read_parquet(parquet_file)
+    for item in data.values.tolist():
+        try:
+            select_query = f"""
+                SELECT * FROM dim_location
+                WHERE location_id = {literal(item[0])}"""
+            print(select_query)
+            select_result = conn.run(select_query)
+            if len(select_result) == 0:
+                if type(item[2]) is not str:
+                    item[2] = None
+                if type(item[3]) is not str:
+                    item[3] = None
+                query = f"""INSERT INTO dim_location (
+                        location_id, address_line_1,
+                        address_line_2, district,
+                        city, postal_code,
+                        country, phone
+                        ) VALUES (
+                        {literal(item[0])}, {literal(item[1])},
+                        {literal(item[2])}, {literal(item[3])},
+                        {literal(item[4])}, {literal(item[5])},
+                        {literal(item[6])}, {literal(item[7])}
+                        )"""
+                conn.run(query)
+        except DatabaseError as d:
+            raise d
+        except Exception as e:
+            raise e
+        except ValueError as v:
+            logger.error(f"Load handler has raised an error: {v}")
+            raise v
+    return "Data loaded successfully - dim_location"
--- tests/process_tests/test_transform_address.py
+++ tests/process_tests/test_transform_address.py
@@ -1,110 +1,21 @@
 from src.process.process_utils.transform_address import transform_address
-from pg8000.native import Connection
-import pytest
 import pandas as pd
-from tests.get_credentials import get_credentials
-import subprocess
-from dotenv import dotenv_values
-
-identity = subprocess.check_output('whoami')
-
-if identity == b'runner\n':
-    config = get_credentials('test_totesys_db_creds')
-else:
-    config = dotenv_values('.env')
-
-user = config["TESTDW_USER"]
-password = config["TESTDW_PASSWORD"]
-host = config["TESTDW_HOST"]
-port = config["TESTDW_PORT"]
-database = config["TESTDW_DATABASE"]
 
 
-@pytest.fixture
-def conn():
-    return Connection(
-        user=user,
-        password=password,
-        host=host,
-        port=port,
-        database=database)
-
-
-def test_function_returns_data_frame(conn):
-    result = transform_address(
-       'tests/csv_test_files/test-address.csv',
-       conn
-    )
+def test_function_returns_data_frame():
+    result = transform_address("tests/csv_test_files/test-address.csv")
     assert isinstance(result, pd.core.frame.DataFrame)
 
 
-def test_function_returns_correct_data(conn):
-    result = transform_address(
-       'tests/csv_test_files/test-address.csv',
-       conn
-    )
+def test_function_returns_correct_data():
+    result = transform_address("tests/csv_test_files/test-address.csv")
     assert result.values.tolist()[3] == [
         4,
-        '5 Far Lane',
-        'Parkway',
-        'North Shore',
-        'Castletown',
-        'AB2 3CD',
-        'Wales',
-        '1234 800900'
+        "5 Far Lane",
+        "Parkway",
+        "North Shore",
+        "Castletown",
+        "AB2 3CD",
+        "Wales",
+        "1234 800900",
     ]
-
-
-def test_function_inserts_correct_data(conn):
-    transform_address(
-        'tests/csv_test_files/test-address.csv',
-        conn
-    )
-    select_query = '''SELECT * FROM dim_location
-    WHERE location_id = 4;'''
-    result = conn.run(select_query)
-    assert result == [[
-        4,
-        '5 Far Lane',
-        'Parkway',
-        'North Shore',
-        'Castletown',
-        'AB2 3CD',
-        'Wales',
-        '1234 800900'
-    ]]
-
-
-def test_function_replaces_empty_values_with_none(conn):
-    transform_address(
-       'tests/csv_test_files/test-address.csv',
-       conn
-    )
-    select_query = '''SELECT * FROM dim_location
-    WHERE location_id = 2;'''
-    result = conn.run(select_query)
-    assert result == [[
-        2,
-        "234 St. Steven's Road",
-        None,
-        None,
-        'Old Town',
-        '22222-3333',
-        'Northern Ireland',
-        '07700 100200'
-    ]]
-
-
-def test_function_does_not_duplicate_data(conn):
-    transform_address(
-        'tests/csv_test_files/test-address.csv',
-        conn
-    )
-    transform_address(
-        'tests/csv_test_files/test-address.csv',
-        conn
-    )
-    result = conn.run(
-        'SELECT * FROM dim_location;'
-    )
-    assert len(result) == 5
