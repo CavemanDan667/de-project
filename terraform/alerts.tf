f6356eee5e7fee7ae55834db5efc9276ec4f477b
CavemanDan667 <dancox82@gmail.com>
2023-11-10 16:21:19 +0000

    tweaked alerts.tf so we have separate alarms for each lambda

--- terraform/alerts.tf
+++ terraform/alerts.tf
@@ -20,88 +20,114 @@ resource "aws_sns_topic_subscription" "sns_subscription" {
 ############################### INGESTION LAMBA ###############################
 # Creates a CloudWatch Log Metric Filter resource - 'Error' Filter
 resource "aws_cloudwatch_log_metric_filter" "ingestion_error_filter" {
-  name           = "ErrorFilter"
+  name           = "IngestionErrorFilter"
   pattern        = "ERROR"
   log_group_name = "/aws/lambda/ingestion_lambda"
 
   metric_transformation {
-    name      = "ErrorCount"
+    name      = "IngestionErrorCount"
     namespace = "CustomLambdaMetrics"
     value     = "1"
   }
 }
 
+# Sets an alarm monitoring the Error metric
+resource "aws_cloudwatch_metric_alarm" "ingestion_error_alarm" {
+  alarm_name = "IngestionErrorAlarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods = 1
+  threshold = 0
+  period = 600
+  metric_name = "IngestionErrorCount"
+  statistic = "SampleCount"
+  alarm_actions = [aws_sns_topic.alerts.arn]
+  namespace = "CustomLambdaMetrics"
+  treat_missing_data = "notBreaching"
+}
+
 # Creates a CloudWatch Log Metric Filter resource - 'Created' Filter
 resource "aws_cloudwatch_log_metric_filter" "created_filter" {
-  name           = "CreatedFilter"
+  name           = "IngestionCreatedFilter"
   pattern        = "CREATED"
   log_group_name = "/aws/lambda/ingestion_lambda"
 
   metric_transformation {
-    name      = "CreatedCount"
+    name      = "IngestionCreatedCount"
     namespace = "CustomLambdaMetrics"
     value     = "1"
   }
+}
+
+# Sets an alarm monitoring the Created metric
+resource "aws_cloudwatch_metric_alarm" "ingestion_created_alarm" {
+  alarm_name = "IngestionCreatedAlarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods = 6
+  datapoints_to_alarm = 5
+  threshold = 0
+  period = 600
+  metric_name = "IngestionCreatedCount"
+  statistic = "SampleCount"
+  alarm_actions = [aws_sns_topic.alerts.arn]
+  namespace = "CustomLambdaMetrics"
+  treat_missing_data = "notBreaching"
 }
 
 
 ############################### PROCESS LAMBA ###############################
 # Creates a CloudWatch Log Metric Filter resource - 'Error' Filter
 resource "aws_cloudwatch_log_metric_filter" "process_error_filter" {
-  name           = "ErrorFilter"
+  name           = "ProcessErrorFilter"
   pattern        = "ERROR"
   log_group_name = "/aws/lambda/process_lambda"
 
   metric_transformation {
-    name      = "ErrorCount"
+    name      = "ProcessErrorCount"
     namespace = "CustomLambdaMetrics"
     value     = "1"
   }
 }
+
+# Sets an alarm monitoring the Error metric
+resource "aws_cloudwatch_metric_alarm" "process_error_alarm" {
+  alarm_name = "ProcessErrorAlarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods = 1
+  threshold = 0
+  period = 600
+  metric_name = "ProcessErrorCount"
+  statistic = "SampleCount"
+  alarm_actions = [aws_sns_topic.alerts.arn]
+  namespace = "CustomLambdaMetrics"
+  treat_missing_data = "notBreaching"
+}
+
 
 
 ############################### LOAD LAMBA ###############################
 # Creates a CloudWatch Log Metric Filter resource - 'Error' Filter
 resource "aws_cloudwatch_log_metric_filter" "load_error_filter" {
-  name           = "ErrorFilter"
+  name           = "LoadErrorFilter"
   pattern        = "ERROR"
   log_group_name = "/aws/lambda/load_lambda"
 
   metric_transformation {
-    name      = "ErrorCount"
+    name      = "LoadErrorCount"
     namespace = "CustomLambdaMetrics"
     value     = "1"
   }
 }
 
-
-############################### ALARMS ###############################
 # Sets an alarm monitoring the Error metric
-resource "aws_cloudwatch_metric_alarm" "error_alarm" {
-  alarm_name = "ErrorAlarm"
+resource "aws_cloudwatch_metric_alarm" "load_error_alarm" {
+  alarm_name = "LoadErrorAlarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods = 1
   threshold = 0
   period = 600
-  metric_name = "ErrorCount"
+  metric_name = "LoadErrorCount"
   statistic = "SampleCount"
   alarm_actions = [aws_sns_topic.alerts.arn]
   namespace = "CustomLambdaMetrics"
   treat_missing_data = "notBreaching"
 }
-
-# Sets an alarm monitoring the Created metric
-resource "aws_cloudwatch_metric_alarm" "created_alarm" {
-  alarm_name = "CreatedAlarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = 6
-  datapoints_to_alarm = 5
-  threshold = 0
-  period = 600
-  metric_name = "CreatedCount"
-  statistic = "SampleCount"
-  alarm_actions = [aws_sns_topic.alerts.arn]
-  namespace = "CustomLambdaMetrics"
-  treat_missing_data = "notBreaching"
-}
-
