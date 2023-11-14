093d83c1086df6d6d2d69d8944da205dc59626be
Wander1ngPanda <jack@ukmurphy.com>
2023-11-14 12:09:30 +0000

    get-time and associated test deleted, ingestion.py docstring updated

--- tests/ingestion_tests/test_get_time.py
+++
@@ -1,8 +0,0 @@
-from src.ingestion.ingestion_utils.get_time import get_time
-from freezegun import freeze_time
-from datetime import datetime
-
-
-@freeze_time("2020-01-01 00:05:00")
-def test_should_return_the_current_time():
-    assert get_time() == datetime(2020, 1, 1, 0, 5, 0)
