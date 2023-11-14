3dc4935688894d0b96fef8d769b6abd064ab02a3
Jason <jasoncolville@hotmail.co.uk>
2023-11-14 09:26:52 +0000

    load and transform transaction plus testing suites

---
+++ tests/load_tests/test_load_transaction.py
@@ -0,0 +1,174 @@
+from src.loading.load_utils.load_transaction import load_transaction
+from src.loading.load_utils.load_sales_order import load_sales_order
+from src.loading.load_utils.load_purchase_order import load_purchase_order
+from src.loading.load_utils.load_design import load_design
+from src.loading.load_utils.load_address import load_address
+from src.loading.load_utils.load_counterparty import load_counterparty
+from src.loading.load_utils.load_currency import load_currency
+from src.loading.load_utils.load_staff import load_staff
+from src.loading.load_utils.get_credentials import get_credentials
+from pg8000.native import Connection
+from dotenv import dotenv_values
+import pytest
+import subprocess
+
+identity = subprocess.check_output("whoami")
+
+if identity == b"runner\n":
+    config = get_credentials("test_dw_creds")
+else:
+    config = dotenv_values(".env")
+
+user = config["TESTDW_USER"]
+password = config["TESTDW_PASSWORD"]
+host = config["TESTDW_HOST"]
+port = config["TESTDW_PORT"]
+database = config["TESTDW_DATABASE"]
+
+
+@pytest.fixture()
+def conn():
+    return Connection(
+        user=user, password=password, host=host, port=port, database=database
+    )
+
+
+def test_function_returns_success_message(conn):
+    load_design(
+       's3://de-project-test-data/parquet/test-design.parquet',
+       conn
+    )
+    load_staff(
+       's3://de-project-test-data/parquet/staff-test.parquet',
+       conn
+    )
+    load_address(
+       's3://de-project-test-data/parquet/test-address.parquet',
+       conn
+    )
+    load_counterparty(
+        's3://de-project-test-data/parquet/test-counterparty.parquet',
+        conn
+    )
+    load_currency(
+        's3://de-project-test-data/parquet/test-currency.parquet',
+        conn
+    )
+    load_sales_order(
+        "s3://de-project-test-data/parquet/test-sales-order.parquet",
+        conn
+    )
+    load_purchase_order(
+        's3://de-project-test-data/parquet/test-purchase-order.parquet',
+        conn
+    )
+    result = load_transaction(
+        "s3://de-project-test-data/parquet/test-transaction.parquet",
+        conn
+    )
+    
+    assert result == "Data loaded successfully - dim_transaction"
+
+
+def test_function_correctly_populates_table(conn):
+    load_design(
+       's3://de-project-test-data/parquet/test-design.parquet',
+       conn
+    )
+    load_staff(
+       's3://de-project-test-data/parquet/staff-test.parquet',
+       conn
+    )
+    load_address(
+       's3://de-project-test-data/parquet/test-address.parquet',
+       conn
+    )
+    load_counterparty(
+        's3://de-project-test-data/parquet/test-counterparty.parquet',
+        conn
+    )
+    load_currency(
+        's3://de-project-test-data/parquet/test-currency.parquet',
+        conn
+    )
+    load_sales_order(
+        "s3://de-project-test-data/parquet/test-sales-order.parquet",
+        conn
+    )
+    load_purchase_order(
+        's3://de-project-test-data/parquet/test-purchase-order.parquet',
+        conn
+    )
+    load_transaction(
+        "s3://de-project-test-data/parquet/test-transaction.parquet",
+        conn
+    )
+    result = conn.run("SELECT * FROM dim_transaction;")
+    assert result == [[1, 'PURCHASE', None, 2],
+                      [2, 'PURCHASE', None, 3],
+                      [3, 'SALE', 8, None],
+                      [4, 'PURCHASE', None, 1],
+                      [5, 'PURCHASE', None, 4],
+                      [6, 'SALE', 2, None],
+                      [7, 'SALE', 3, None],
+                      [8, 'PURCHASE', None, 5],
+                      [9, 'SALE', 5, None], 
+                      [10, 'SALE', 4, None]]
+
+
+def test_function_does_not_duplicate_data(conn):
+    load_design(
+       's3://de-project-test-data/parquet/test-design.parquet',
+       conn
+    )
+    load_staff(
+       's3://de-project-test-data/parquet/staff-test.parquet',
+       conn
+    )
+    load_address(
+       's3://de-project-test-data/parquet/test-address.parquet',
+       conn
+    )
+    load_counterparty(
+        's3://de-project-test-data/parquet/test-counterparty.parquet',
+        conn
+    )
+    load_currency(
+        's3://de-project-test-data/parquet/test-currency.parquet',
+        conn
+    )
+    load_sales_order(
+        "s3://de-project-test-data/parquet/test-sales-order.parquet",
+        conn
+    )
+    load_purchase_order(
+        's3://de-project-test-data/parquet/test-purchase-order.parquet',
+        conn
+    )
+    load_transaction(
+        "s3://de-project-test-data/parquet/test-transaction.parquet",
+        conn
+    )
+    load_transaction(
+        "s3://de-project-test-data/parquet/test-transaction.parquet",
+        conn
+    )
+    load_transaction(
+        "s3://de-project-test-data/parquet/test-transaction.parquet",
+        conn
+    )
+    load_transaction(
+        "s3://de-project-test-data/parquet/test-transaction.parquet",
+        conn
+    )
+    result = conn.run("SELECT * FROM dim_transaction;")
+    assert result == [[1, 'PURCHASE', None, 2],
+                      [2, 'PURCHASE', None, 3],
+                      [3, 'SALE', 8, None],
+                      [4, 'PURCHASE', None, 1],
+                      [5, 'PURCHASE', None, 4],
+                      [6, 'SALE', 2, None],
+                      [7, 'SALE', 3, None],
+                      [8, 'PURCHASE', None, 5],
+                      [9, 'SALE', 5, None], 
+                      [10, 'SALE', 4, None]]
